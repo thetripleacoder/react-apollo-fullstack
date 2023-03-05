996e384e1ffb6186cef3fabce6ebf7312da06834
thetripleacoder <85655422+thetripleacoder@users.noreply.github.com>
2023-03-05 13:53:40 +0800

    Initial commit for client app

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+  ApolloClient - providers
+  InMemoryCache - cache query results after fetching them
+  ApolloProvider - 
+  HttpLink - 
+  from - method
+*/
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+  mode: 'no-cors',
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query getAllPosts {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
